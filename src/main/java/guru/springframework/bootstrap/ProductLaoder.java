7edcd4e079cec64b8667d25ebe84639bb24b8f87
hevoda <herve.evoda@ipsedocet.it>
2025-11-20 06:19:07 +0100

    running project

--- src/main/java/guru/springframework/bootstrap/ProductLaoder.java
+++ src/main/java/guru/springframework/bootstrap/ProductLaoder.java
@@ -35,7 +35,7 @@ public class ProductLaoder implements ApplicationListener<ContextRefreshedEvent>
         shirt.setDescription("Spring Framework Guru Shirt");
         shirt.setPrice(new BigDecimal("18.95"));
         shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setId(235268845711068308L);
+
         productRepository.save(shirt);
 
         log.info("Saved Shirt - id: " + shirt.getId());
@@ -43,7 +43,7 @@ public class ProductLaoder implements ApplicationListener<ContextRefreshedEvent>
         ProductEntity mug = new ProductEntity();
         mug.setDescription("Spring Framework Guru Mug");
         mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setId(168639393495335947L);
+
         mug.setPrice(new BigDecimal("11.95"));
         productRepository.save(mug);
 
